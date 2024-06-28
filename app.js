1dea45f4e1eec30a3ca910a7516248dce0c43de6
Diallo <mamadouadama609@gmail.com>
2024-06-28 14:14:39 -0500

    added routes module

---
+++ app.js
@@ -0,0 +1,16 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import userRouter from './routes/users.js'
+
+
+const PORT = 3000
+const app = express();
+
+app.use(bodyParser.json())
+
+app.use('/users', userRouter);
+
+
+
+
+app.listen(PORT, () => console.log(`Server is running on http://localhost:${PORT}`));
